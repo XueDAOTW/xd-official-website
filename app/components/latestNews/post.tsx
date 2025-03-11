ebee5bae827610c0fca38ec4a128ef53bd2ca3c7
crypto0627 <jake0627a1@gmail.com>
2025-03-11 23:44:30 +0800

    fix: RWD error

--- app/components/latestNews/post.tsx
+++ app/components/latestNews/post.tsx
@@ -8,15 +8,15 @@ const instagramPosts = [
 
 function Post() {
   return (
-    <div className="flex w-full bg-gradient-to-b py-12">
-      <div className="container flex flex-col mx-auto w-full">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full">
+    <div className="flex w-full bg-gradient-to-b py-6 md:py-12">
+      <div className="container flex flex-col mx-auto w-full px-4 md:px-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 md:gap-6 w-full">
           {instagramPosts.map((postUrl, index) => (
-            <div key={index} className="flex w-full justify-center items-center">
+            <div key={index} className="flex w-full justify-center items-center md:p-4">
               <InstagramEmbed 
                 url={postUrl}
                 width="100%"
-                className="w-full"
+                className="w-full min-w-[280px] max-w-[390px] md:max-w-[450px] xl:max-w-[400px]"
                 style={{ margin: "auto" }}
               />
             </div>
